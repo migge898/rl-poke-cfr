63f5bb04265f8b3cc8ce0f26d520f6b536dea484
Miguel Mioskowski <migge898@gmx.de>
2026-06-01 16:56:44 +0200

    added teams list from smogon dpp ou sample teams

---
+++ teams.py
@@ -0,0 +1,747 @@
+
+# taken from https://www.smogon.com/forums/threads/dpp-ou-sample-teams-v2.3767939/
+TEAMS = [
+    {
+        "name": "Azelf + Triple DD + Swords Dance Empoleon",
+        "tags": ["hyper-offensive", "offensive"],
+        "pokemon": """
+Azelf @ Focus Sash  
+Ability: Levitate  
+EVs: 160 HP / 96 Atk / 252 Spe  
+Jolly Nature  
+- Taunt  
+- Stealth Rock  
+- Thunder Wave  
+- Explosion  
+
+Gyarados @ Wacan Berry  
+Ability: Intimidate  
+EVs: 8 HP / 252 Atk / 40 SpD / 208 Spe  
+Adamant Nature  
+- Waterfall  
+- Earthquake  
+- Ice Fang  
+- Dragon Dance  
+
+Dragonite @ Life Orb  
+Ability: Inner Focus  
+EVs: 24 HP / 252 Atk / 16 SpD / 216 Spe  
+Adamant Nature  
+- Dragon Dance  
+- Outrage  
+- Fire Punch  
+- Extreme Speed  
+
+Tyranitar @ Lum Berry  
+Ability: Sand Stream  
+EVs: 8 HP / 248 Atk / 252 Spe  
+Jolly Nature  
+- Dragon Dance  
+- Crunch  
+- Fire Punch  
+- Aqua Tail  
+
+Metagross @ Lum Berry  
+Ability: Clear Body  
+EVs: 56 HP / 252 Atk / 4 SpD / 196 Spe  
+Adamant Nature  
+- Agility  
+- Meteor Mash  
+- Earthquake  
+- Explosion  
+
+Empoleon @ Sea Incense  
+Ability: Torrent  
+EVs: 32 HP / 252 Atk / 224 Spe  
+Adamant Nature  
+- Swords Dance  
+- Waterfall  
+- Drill Peck  
+- Aqua Jet  
+        """
+    },
+    {
+        "name": "SubSplit Gengar + Magnezone",
+        "tags": ["bulky", "offensive"],
+        "pokemon": """
+Azelf @ Focus Sash  
+Ability: Levitate  
+EVs: 160 HP / 96 Atk / 252 Spe  
+Jolly Nature  
+- Taunt  
+- Stealth Rock  
+- Thunder Wave  
+- Explosion  
+
+Gyarados @ Wacan Berry  
+Ability: Intimidate  
+EVs: 8 HP / 252 Atk / 40 SpD / 208 Spe  
+Adamant Nature  
+- Waterfall  
+- Earthquake  
+- Ice Fang  
+- Dragon Dance  
+
+Dragonite @ Life Orb  
+Ability: Inner Focus  
+EVs: 24 HP / 252 Atk / 16 SpD / 216 Spe  
+Adamant Nature  
+- Dragon Dance  
+- Outrage  
+- Fire Punch  
+- Extreme Speed  
+
+Tyranitar @ Lum Berry  
+Ability: Sand Stream  
+EVs: 8 HP / 248 Atk / 252 Spe  
+Jolly Nature  
+- Dragon Dance  
+- Crunch  
+- Fire Punch  
+- Aqua Tail  
+
+Metagross @ Lum Berry  
+Ability: Clear Body  
+EVs: 56 HP / 252 Atk / 4 SpD / 196 Spe  
+Adamant Nature  
+- Agility  
+- Meteor Mash  
+- Earthquake  
+- Explosion  
+
+Empoleon @ Sea Incense  
+Ability: Torrent  
+EVs: 32 HP / 252 Atk / 224 Spe  
+Adamant Nature  
+- Swords Dance  
+- Waterfall  
+- Drill Peck  
+- Aqua Jet  
+
+"""
+    },
+    {
+        "name": "Empoleon Lead Specs Kingdra Water Spam",
+        "tags": ["bulky", "offensive"],
+        "pokemon": """
+Empoleon @ Focus Sash  
+Ability: Torrent  
+EVs: 244 Atk / 252 SpA / 12 Spe  
+Rash Nature  
+- Hydro Pump  
+- Ice Beam  
+- Aqua Jet  
+- Grass Knot  
+
+Kingdra @ Choice Specs  
+Ability: Swift Swim  
+EVs: 4 Def / 252 SpA / 252 Spe  
+Modest Nature  
+IVs: 0 Atk  
+- Hydro Pump  
+- Draco Meteor  
+- Surf  
+- Dragon Pulse  
+
+Gyarados @ Choice Band  
+Ability: Intimidate  
+EVs: 252 Atk / 4 Def / 252 Spe  
+Adamant Nature  
+- Waterfall  
+- Earthquake  
+- Ice Fang  
+- Outrage  
+
+Metagross @ Leftovers  
+Ability: Clear Body  
+EVs: 252 HP / 4 Def / 252 SpD  
+Careful Nature  
+- Stealth Rock  
+- Meteor Mash  
+- Earthquake  
+- Explosion  
+
+Latias (F) @ Choice Scarf  
+Ability: Levitate  
+EVs: 80 HP / 252 SpA / 176 Spe  
+Timid Nature  
+IVs: 0 Atk  
+- Draco Meteor  
+- Thunderbolt  
+- Trick  
+- Healing Wish  
+
+Tyranitar (F) @ Lum Berry  
+Ability: Sand Stream  
+EVs: 48 HP / 188 Atk / 4 SpA / 64 SpD / 204 Spe  
+Lonely Nature  
+- Crunch  
+- Pursuit  
+- Fire Blast  
+- Superpower  
+
+        """
+    },
+    {
+        "name": "Skarmory Lead + Infernape",
+        "tags": ["hazard stack", "offensive"],
+        "pokemon": """
+Skarmory @ Focus Sash  
+Ability: Keen Eye  
+EVs: 252 HP / 4 SpA / 252 Spe  
+Timid Nature  
+IVs: 0 Atk  
+- Spikes  
+- Stealth Rock  
+- Taunt  
+- Icy Wind  
+
+Heatran @ Shuca Berry  
+Ability: Flash Fire  
+Hidden Power: Ice  
+EVs: 252 SpA / 4 SpD / 252 Spe  
+Timid Nature  
+- Fire Blast  
+- Earth Power  
+- Explosion  
+- Taunt  
+
+Rotom-Heat @ Choice Scarf  
+Ability: Levitate  
+EVs: 16 HP / 240 SpA / 252 Spe  
+Timid Nature  
+- Thunderbolt  
+- Overheat  
+- Shadow Ball  
+- Trick  
+
+Scizor @ Choice Band  
+Ability: Technician  
+EVs: 248 HP / 252 Atk / 8 SpD  
+Adamant Nature  
+- U-turn  
+- Bullet Punch  
+- Superpower  
+- Pursuit  
+
+Infernape @ Life Orb  
+Ability: Blaze  
+EVs: 64 Atk / 252 SpA / 192 Spe  
+Naive Nature  
+- Fire Blast  
+- Close Combat  
+- U-turn  
+- Slack Off  
+
+Latias (F) @ Choice Specs  
+Ability: Levitate  
+EVs: 4 HP / 80 Def / 248 SpA / 176 Spe  
+Modest Nature  
+IVs: 0 Atk  
+- Draco Meteor  
+- Surf  
+- Thunderbolt  
+- Trick  
+        """
+    },
+    {
+        "name": "Mixed Flygon + Doom Desire Jirachi",
+        "tags": ["hazard stack", "offensive"],
+        "pokemon": """
+Skarmory @ Focus Sash  
+Ability: Keen Eye  
+Shiny: Yes  
+EVs: 252 HP / 4 SpD / 252 Spe  
+Timid Nature  
+IVs: 0 Atk  
+- Stealth Rock  
+- Spikes  
+- Taunt  
+- Icy Wind  
+
+Gengar @ Life Orb  
+Ability: Levitate  
+Shiny: Yes  
+EVs: 4 Atk / 252 SpA / 252 Spe  
+Naive Nature  
+- Shadow Ball  
+- Focus Blast  
+- Sucker Punch  
+- Will-O-Wisp  
+
+Jirachi @ Occa Berry  
+Ability: Serene Grace  
+Shiny: Yes  
+EVs: 72 HP / 4 Atk / 252 SpA / 180 Spe  
+Mild Nature  
+IVs: 30 Def / 30 SpA / 30 Spe  
+- Iron Head  
+- Doom Desire  
+- Thunder Wave  
+- Hidden Power [Fire]  
+
+Flygon @ Life Orb  
+Ability: Levitate  
+Shiny: Yes  
+EVs: 252 Atk / 4 SpA / 252 Spe  
+Hasty Nature  
+- Earthquake  
+- Outrage  
+- Fire Blast  
+- Toxic  
+
+Latias @ Choice Scarf  
+Ability: Levitate  
+Shiny: Yes  
+EVs: 4 Def / 252 SpA / 252 Spe  
+Timid Nature  
+IVs: 0 Atk  
+- Draco Meteor  
+- Thunder Wave  
+- Trick  
+- Healing Wish  
+
+Tyranitar @ Chople Berry  
+Ability: Sand Stream  
+Shiny: Yes  
+EVs: 252 Atk / 4 Def / 252 Spe  
+Jolly Nature  
+- Dragon Dance  
+- Crunch  
+- Aqua Tail  
+- Fire Punch  
+        """
+    },
+    {
+        "name": "Double Water",
+        "tags": ["hazard stack", "offensive"],
+        "pokemon": """
+Skarmory @ Focus Sash  
+Ability: Keen Eye  
+EVs: 252 HP / 4 Atk / 252 Spe  
+Jolly Nature  
+- Stealth Rock  
+- Spikes  
+- Taunt  
+- Brave Bird  
+
+Dragonite @ Choice Band  
+Ability: Inner Focus  
+EVs: 252 Atk / 20 Def / 236 Spe  
+Jolly Nature  
+- Outrage  
+- Superpower  
+- Extreme Speed  
+- Sleep Talk  
+
+Suicune @ Leftovers  
+Ability: Pressure  
+EVs: 4 HP / 252 SpA / 252 Spe  
+Timid Nature  
+IVs: 0 Atk  
+- Calm Mind  
+- Surf  
+- Ice Beam  
+- Protect  
+
+Rotom-Wash @ Choice Scarf  
+Ability: Levitate  
+EVs: 252 SpA / 4 SpD / 252 Spe  
+Timid Nature  
+IVs: 2 Atk / 30 Def  
+- Thunderbolt  
+- Shadow Ball  
+- Hidden Power [Ice]  
+- Trick  
+
+Swampert @ Choice Band  
+Ability: Torrent  
+EVs: 252 Atk / 4 SpD / 252 Spe  
+Adamant Nature  
+- Waterfall  
+- Earthquake  
+- Superpower  
+- Ice Punch  
+
+Heatran @ Choice Scarf  
+Ability: Flash Fire  
+EVs: 4 Atk / 252 SpA / 252 Spe  
+Mild Nature  
+- Fire Blast  
+- Earth Power  
+- Dragon Pulse  
+- Explosion  
+        """
+    },
+    {
+        "name": "TrickScarf Uxie ZapMie",
+        "tags": ["zapmie", "balance"],
+        "pokemon": """
+Uxie @ Choice Scarf  
+Ability: Levitate  
+EVs: 252 HP / 104 Def / 152 Spe  
+Impish Nature  
+- Trick  
+- Stealth Rock  
+- Thunder Wave  
+- U-turn  
+
+Tyranitar @ Choice Band  
+Ability: Sand Stream  
+EVs: 176 HP / 252 Atk / 64 SpD / 16 Spe  
+Adamant Nature  
+- Pursuit  
+- Crunch  
+- Stone Edge  
+- Superpower  
+
+Starmie @ Leftovers  
+Ability: Natural Cure  
+EVs: 248 HP / 44 Def / 216 Spe  
+Timid Nature  
+IVs: 0 Atk  
+- Surf  
+- Thunder Wave  
+- Rapid Spin  
+- Recover  
+
+Swampert @ Leftovers  
+Ability: Torrent  
+EVs: 240 HP / 252 Atk / 16 Spe  
+Adamant Nature  
+- Waterfall  
+- Earthquake  
+- Ice Punch  
+- Protect  
+
+Zapdos @ Leftovers  
+Ability: Pressure  
+EVs: 248 HP / 224 Def / 36 SpD  
+Bold Nature  
+- Thunderbolt  
+- Hidden Power [Ice]  
+- U-turn  
+- Roost  
+
+Jirachi @ Leftovers  
+Ability: Serene Grace  
+EVs: 252 HP / 108 Atk / 148 Spe  
+Jolly Nature  
+- Substitute  
+- Iron Head  
+- Body Slam  
+- Fire Punch  
+        """
+    },
+    {
+        "name": "SubSplit Gengar + Magnezone",
+        "tags": ["zapmie", "balance"],
+        "pokemon": """
+        """
+    },
+    {
+        "name": "Double Ground Mag Balance",
+        "tags": ["mag balance", "balance"],
+        "pokemon": """
+Hippowdon @ Leftovers  
+Ability: Sand Stream  
+EVs: 248 HP / 8 Def / 252 SpD  
+Careful Nature  
+- Stealth Rock  
+- Earthquake  
+- Slack Off  
+- Roar  
+
+Jirachi @ Leftovers  
+Ability: Serene Grace  
+EVs: 248 HP / 168 Def / 60 SpD / 32 Spe  
+Impish Nature  
+- Iron Head  
+- Body Slam  
+- Wish  
+- Protect  
+
+Magneton @ Choice Scarf  
+Ability: Magnet Pull  
+EVs: 252 SpA / 4 SpD / 252 Spe  
+Timid Nature  
+IVs: 2 Atk / 30 Def  
+- Thunderbolt  
+- Hidden Power [Ice]  
+- Thunder Wave  
+- Flash Cannon  
+
+Gliscor @ Leftovers  
+Ability: Hyper Cutter  
+EVs: 248 HP / 44 Def / 216 Spe  
+Impish Nature  
+- Taunt  
+- Earthquake  
+- Fire Fang  
+- Roost  
+
+Clefable @ Leftovers  
+Ability: Magic Guard  
+EVs: 252 HP / 52 Def / 204 SpD  
+Careful Nature  
+- Knock Off  
+- Seismic Toss  
+- Soft-Boiled  
+- Thunder Wave  
+
+Latias (F) @ Leftovers  
+Ability: Levitate  
+EVs: 248 HP / 204 Def / 56 Spe  
+Bold Nature  
+IVs: 0 Atk  
+- Toxic  
+- Reflect  
+- Dragon Pulse  
+- Recover  
+        """
+    },
+    {
+        "name": "NidoMilo + Calm Mind Clef Mag Balance",
+        "tags": ["mag balance", "balance"],
+        "pokemon": """
+Bronzong @ Leftovers  
+Ability: Levitate  
+EVs: 248 HP / 128 Def / 132 SpD  
+Sassy Nature  
+- Stealth Rock  
+- Protect  
+- Payback  
+- Earthquake  
+
+Nidoqueen (F) @ Black Sludge  
+Ability: Poison Point  
+EVs: 248 HP / 252 Def / 8 Spe  
+Relaxed Nature  
+- Protect  
+- Earthquake  
+- Flamethrower  
+- Toxic Spikes  
+
+Magnezone @ Choice Scarf  
+Ability: Magnet Pull  
+EVs: 252 SpA / 4 SpD / 252 Spe  
+Timid Nature  
+IVs: 2 Atk / 30 SpA / 30 Spe  
+- Thunderbolt  
+- Thunder Wave  
+- Hidden Power [Fire]  
+- Flash Cannon  
+
+Milotic @ Leftovers  
+Ability: Marvel Scale  
+EVs: 248 HP / 252 Def / 8 SpD  
+Bold Nature  
+IVs: 0 Atk  
+- Surf  
+- Toxic  
+- Haze  
+- Recover  
+
+Latias @ Leftovers  
+Ability: Levitate  
+EVs: 248 HP / 204 Def / 56 Spe  
+Bold Nature  
+IVs: 0 Atk  
+- Calm Mind  
+- Dragon Pulse  
+- Thunder Wave  
+- Recover  
+
+Clefable @ Leftovers  
+Ability: Magic Guard  
+EVs: 252 HP / 252 Def / 4 SpA  
+Bold Nature  
+- Calm Mind  
+- Thunderbolt  
+- Ice Beam  
+- Soft-Boiled  
+        """
+    },
+    {
+        "name": "I Crawled",
+        "tags": ["semi-stall", "stall"],
+        "pokemon": """
+Tyranitar @ Leftovers  
+Ability: Sand Stream  
+EVs: 252 HP / 4 Atk / 252 SpD  
+Careful Nature  
+- Stealth Rock  
+- Crunch  
+- Protect  
+- Roar  
+
+Skarmory @ Leftovers  
+Ability: Keen Eye  
+EVs: 252 HP / 252 Def / 4 SpD  
+Impish Nature  
+- Spikes  
+- Whirlwind  
+- Roost  
+- Brave Bird  
+
+Nidoqueen (F) @ Black Sludge  
+Ability: Poison Point  
+EVs: 252 HP / 252 Def / 4 SpA  
+Bold Nature  
+IVs: 0 Atk  
+- Protect  
+- Toxic Spikes  
+- Ice Beam  
+- Roar  
+
+Clefable @ Leftovers  
+Ability: Magic Guard  
+EVs: 252 HP / 4 Def / 252 SpD  
+Careful Nature  
+- Wish  
+- Protect  
+- Seismic Toss  
+- Knock Off  
+
+Jirachi @ Leftovers  
+Ability: Serene Grace  
+EVs: 80 HP / 252 Atk / 176 Spe  
+Adamant Nature  
+- Iron Head  
+- Body Slam  
+- Fire Punch  
+- Protect  
+
+Rotom-Heat @ Choice Scarf  
+Ability: Levitate  
+EVs: 16 HP / 252 SpA / 4 SpD / 236 Spe  
+Timid Nature  
+IVs: 2 Atk / 30 Def  
+- Thunderbolt  
+- Hidden Power [Ice]  
+- Overheat  
+- Trick  
+        """
+    },
+    {
+        "name": "Standard Hippo",
+        "tags": ["semi-stall", "stall"],
+        "pokemon": """
+Hippowdon @ Leftovers  
+Ability: Sand Stream  
+EVs: 252 HP / 252 SpD / 4 Spe  
+Careful Nature  
+- Stealth Rock  
+- Slack Off  
+- Earthquake  
+- Roar  
+
+Latias (F) @ Leftovers  
+Ability: Levitate  
+EVs: 252 HP / 160 Def / 96 Spe  
+Bold Nature  
+IVs: 0 Atk  
+- Thunder Wave  
+- Dragon Pulse  
+- Reflect  
+- Recover  
+
+Skarmory @ Leftovers  
+Ability: Keen Eye  
+EVs: 252 HP / 252 Def / 4 SpD  
+Impish Nature  
+- Spikes  
+- Roost  
+- Whirlwind  
+- Brave Bird  
+
+Clefable @ Leftovers  
+Ability: Magic Guard  
+EVs: 252 HP / 4 Def / 252 SpD  
+Careful Nature  
+- Knock Off  
+- Thunder Wave  
+- Seismic Toss  
+- Soft-Boiled  
+
+Rotom-Heat @ Leftovers  
+Ability: Levitate  
+EVs: 252 HP / 252 Def / 4 SpD  
+Bold Nature  
+IVs: 0 Atk  
+- Thunderbolt  
+- Will-O-Wisp  
+- Rest  
+- Sleep Talk  
+
+Jirachi @ Leftovers  
+Ability: Serene Grace  
+EVs: 240 HP / 16 Atk / 252 Spe  
+Jolly Nature  
+- Iron Head  
+- Body Slam  
+- Fire Punch  
+- Protect  
+        """
+    },
+    {
+        "name": "Tyranitar + Gliscor Superman",
+        "tags": ["semi-stall", "stall"],
+        "pokemon": """
+Latias (F) @ Leftovers  
+Ability: Levitate  
+EVs: 252 HP / 156 Def / 100 Spe  
+Timid Nature  
+IVs: 0 Atk  
+- Ice Beam  
+- Thunder Wave  
+- Reflect  
+- Recover  
+
+Bronzong @ Leftovers  
+Ability: Levitate  
+EVs: 252 HP / 84 Def / 172 SpD  
+Sassy Nature  
+- Earthquake  
+- Hidden Power [Fire]  
+- Toxic  
+- Stealth Rock  
+
+Skarmory @ Leftovers  
+Ability: Keen Eye  
+EVs: 252 HP / 248 Def / 8 Spe  
+Impish Nature  
+- Brave Bird  
+- Whirlwind  
+- Spikes  
+- Roost  
+
+Gliscor @ Leftovers  
+Ability: Hyper Cutter  
+EVs: 252 HP / 4 SpD / 252 Spe  
+Jolly Nature  
+- Earthquake  
+- Ice Fang  
+- Taunt  
+- Roost  
+
+Clefable @ Leftovers  
+Ability: Magic Guard  
+EVs: 252 HP / 24 Def / 216 SpD / 16 Spe  
+Careful Nature  
+- Seismic Toss  
+- Knock Off  
+- Encore  
+- Soft-Boiled  
+
+Tyranitar @ Choice Scarf  
+Ability: Sand Stream  
+EVs: 212 Atk / 44 SpA / 252 Spe  
+Hasty Nature  
+- Crunch  
+- Pursuit  
+- Superpower  
+- Fire Blast  
+        """
+    }
+]
