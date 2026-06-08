6c3c6bce40802e333ea0e08b246242782b7de8f0
Miguel Mioskowski <migge898@gmx.de>
2026-06-08 13:02:20 +0200

    deleted tb_test

--- test_tb.py
+++
@@ -1,6 +0,0 @@
-from torch.utils.tensorboard import SummaryWriter
-writer = SummaryWriter("logs/test")
-for i in range(10):
-    writer.add_scalar("Test/Value", i*i, i)
-writer.close()
-print("Test data written to logs/test")
